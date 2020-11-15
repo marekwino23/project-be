027004447be91e48b8662d4f8ee86a6458d08a45
Marek <marekw1996@gmail.com>
2020-11-15 21:14:04 +0100

    setup server 12

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
 var PORT = process.env.PORT || 4000;
 
+const mailer = require("nodemailer");
 const express = require('express');
 const bcrypt = require("bcrypt");
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser')
+const cookieP2arser = require('cookie-parser')
 const db = require('./config/database');
 const handlers = require('./handlers');
+const hbs = require("nodemailer-express-handlebars")
 const app = express();
 const router = express.Router();
+
+
+async function main() {
+  // Generate test SMTP service account from ethereal.email
+  // Only needed if you don't have a real mail account for testing
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "marekw1996@gmail.com, marekw1996@gmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+}
+
+main().catch(console.error);
+
 
 app.use(cors({
     origin: process.env.ORIGIN,
