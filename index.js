63925bee5ff9383977d6aa6487c9b7d628947842
Marek <marekw1996@gmail.com>
2020-12-19 16:12:24 +0100

    setup server 28

--- index.js
+++ index.js
@@ -12,12 +12,20 @@ const app = express();
 const router = express.Router();
 
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
-app.use(cors({
-    origin: process.env.ORIGIN,
-    methods: ['GET','POST','DELETE','PATCH'],
-    credentials: true
-}));
+
+// app.use(cors({
+//     origin: process.env.ORIGIN,
+//     methods: ['GET','POST','DELETE','PATCH'],
+//     credentials: true
+// }));
 
 
 app.use(cookieParser());
