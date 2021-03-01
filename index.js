bbdcff7ecbc60628387c2661f96ae33482094a73
marekwino23 <marekw1996@gmail.com>
2021-03-01 22:08:56 +0100

    heroku logs

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const router = express.Router();
 const corsOptions = {
     origin: (origin, callback) => {
         console.info('info:', process.env.ORIGIN, origin, process.env.NODE_ENV);
-        if(process.env.ORIGIN === origin) {
+        if("https://barber-win.herokuapp.com" === origin) {
             callback(null,true)
         } else {
             console.error("error",process.env.ORIGIN, process.env.NODE_ENV);
